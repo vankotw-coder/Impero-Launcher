41d671cb2d37ee7bcae81480acfd7b9328ae61af
vankotw-coder <vanko.tw@gmail.com>
2025-12-16 17:18:38 +0100

    v1.0.1

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ ipcMain.on('autoUpdateAction', (event, arg, data) => {
             autoUpdater.quitAndInstall()
             break
         default:
-            console.log('Unknown argument', arg)
+            console.log('Argomento sconosciuto', arg)
             break
     }
 })
@@ -269,12 +269,12 @@ function createMenu() {
         let applicationSubMenu = {
             label: 'Application',
             submenu: [{
-                label: 'About Application',
+                label: 'Informazioni sull\'applicazione',
                 selector: 'orderFrontStandardAboutPanel:'
             }, {
                 type: 'separator'
             }, {
-                label: 'Quit',
+                label: 'Esci',
                 accelerator: 'Command+Q',
                 click: () => {
                     app.quit()
@@ -284,31 +284,31 @@ function createMenu() {
 
         // New edit menu adds support for text-editing keyboard shortcuts
         let editSubMenu = {
-            label: 'Edit',
+            label: 'Modifica',
             submenu: [{
-                label: 'Undo',
+                label: 'Disfare',
                 accelerator: 'CmdOrCtrl+Z',
                 selector: 'undo:'
             }, {
-                label: 'Redo',
+                label: 'Rifare',
                 accelerator: 'Shift+CmdOrCtrl+Z',
                 selector: 'redo:'
             }, {
                 type: 'separator'
             }, {
-                label: 'Cut',
+                label: 'Taglio',
                 accelerator: 'CmdOrCtrl+X',
                 selector: 'cut:'
             }, {
-                label: 'Copy',
+                label: 'Copia',
                 accelerator: 'CmdOrCtrl+C',
                 selector: 'copy:'
             }, {
-                label: 'Paste',
+                label: 'Incolla',
                 accelerator: 'CmdOrCtrl+V',
                 selector: 'paste:'
             }, {
-                label: 'Select All',
+                label: 'Seleziona Tutto',
                 accelerator: 'CmdOrCtrl+A',
                 selector: 'selectAll:'
             }]
